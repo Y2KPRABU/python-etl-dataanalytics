7ca772f320369acf685aa408b8ec58c180a8f8d0
sundara prabu <Y2KSUNDARA@YAHOO.COM>
2024-12-18 15:41:42 +0000

    Add Terraform resource configuration

---
+++ terraform/resources.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+  name               = "example-filesystem"
+  storage_account_id = azurerm_storage_account.example.id
+}
