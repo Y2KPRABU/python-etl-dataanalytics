93d86c81710153e371c62f609b6774a3a6930bf5
Sundara Rajan <Y2KSUNDARA@YAHOO.COM>
2025-02-04 18:31:16 +0000

    Update resources.tf with Azure resources based on ARM template

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,14 +1,16 @@
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "East US"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"
+  name                     = "sunadls"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  enable_https_traffic_only = true
+  is_hns_enabled            = true
 
   tags = {
     environment = "staging"
@@ -16,6 +18,18 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
-  name               = "example-filesystem"
+  name               = "sunfs"
   storage_account_id = azurerm_storage_account.example.id
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "sunfs"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_account_network_rules" "example" {
+  storage_account_id = azurerm_storage_account.example.id
+  default_action     = "Allow"
+  bypass             = ["AzureServices"]
 }
